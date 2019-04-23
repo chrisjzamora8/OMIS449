4c427885c6a3ee683066cbbe42850d3c7c00f5ed
chrisjzamora8 <46986611+chrisjzamora8@users.noreply.github.com>
2019-04-22 20:55:30 -0500

    Add files via upload

---
+++ CD Account Test/CD Account Test/Class2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test
+{
+    class Class2
+    {
+    }
+}
---
+++ CD Account Test/CD Account Test/SavingsAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test
+{
+    class SavingsAccount.cs
+    {
+        // Fields
+        private string _accountNumber;
+        private decimal _interestRate;
+        private decimal _balance;
+
+        // Constructor 
+        public SavingsAccount()
+    {
+        _accountNumber = "";
+        _interestRate = 0;
+        _balance = 0;
+    }
+    //AcountNumber Property
+    }
+}
