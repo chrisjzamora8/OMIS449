b0bf1f5116bf0855164b434bc0aae541a6e0744e
chrisjzamora8 <46986611+chrisjzamora8@users.noreply.github.com>
2019-04-22 20:35:28 -0500

    student.cs

--- Chap10/Computer Science Student/Computer Science Student/Student.cs
+++ Chap10/Computer Science Student/Computer Science Student/Student.cs
@@ -5,7 +5,34 @@ using System.Text;
 
 namespace Computer_Science_Student
 {
-    class Student
+   abstract class Student
     {
+        // Fields 
+        private string _name;
+        private string _id;
+
+        // Constructor
+        public Student(string name, string id)
+        {
+            _name = name;
+            _id = id;
+        }
+        // Name property
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        // ID Property
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        // RequiredHours property (abstract)
+        public abstract double RequiredHours
+        { get;
+        }
     }
 }
